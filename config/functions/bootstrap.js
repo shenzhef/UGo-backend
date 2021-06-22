fadb40b0749ccafdb07a0bd749da57789e8b8833
Martin Miauro <martin@capitan.fitness>
2021-06-22 17:48:40 -0300

    api payments v1

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -3,6 +3,33 @@ module.exports = async () => {
     var io = require("socket.io")(strapi.server);
     let users = [];
     let rooms = [];
+    var mercadopago = require("mercadopago");
+    mercadopago.configure({
+      access_token:
+        "TEST-2825597988473691-120214-d4d82f5bbc8b8f574265339f6d4cf0ae-102188289",
+    });
+
+    var preference = {
+      items: [
+        {
+          title: "Paseo de poncho",
+          quantity: 1,
+          currency_id: "ARS",
+          unit_price: 250,
+        },
+      ],
+    };
+
+    mercadopago.preferences
+      .create(preference)
+      .then(function (response) {
+        // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+        console.log(response);
+        global.id = response.body.id;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     io.on("connection", (socket) => {
       console.log("connection");
       if (!users.find((user) => user.socketId == socket.id)) {
