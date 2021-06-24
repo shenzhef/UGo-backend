44b45d8513f136c1e10385d301991a5f34e42bee
Martin Miauro <martin@capitan.fitness>
2021-06-24 12:01:13 -0300

    added transactions

--- api/transaction/controllers/Notifications.js
+++ api/transaction/controllers/Notifications.js
@@ -1,8 +1,13 @@
 "use strict";
+const mercadopago = require("mercadopago");
+mercadopago.configurations.setAccessToken(
+  "TEST-2673649138924674-062117-027521d8ee3db857ed96256a55e4dd4f-102188289"
+);
 
 module.exports = {
   async find(ctx) {
     console.log("aca", ctx.request.body);
+
     return ctx.send("Email enviado!");
   },
 };
--- api/transaction/controllers/transaction.js
+++ api/transaction/controllers/transaction.js
@@ -6,4 +6,26 @@ const { sanitizeEntity } = require("strapi-utils");
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    console.log(id);
+    // const entity = await strapi
+    //   .query("transaction")
+    //   .model.findOne({
+    //     payment_id: id,
+    //   })
+    //   .select("bundleID")
+    //   .populate([
+    //     {
+    //       path: "paseo",
+    //       model: "paseo",
+    //     },
+    //   ]);
+    const entity = await strapi.services.transaction.findOne({
+      payment_id: id,
+    });
+    console.log("entity", entity);
+    return sanitizeEntity(entity, { model: strapi.models.transaction });
+  },
+};
