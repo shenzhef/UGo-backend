b0258432f19d517fcc67a21a209d14e1e6d8e5ff
Martin Miauro <martin@capitan.fitness>
2021-06-28 10:25:58 -0300

    comeback find paseos

--- api/paseo/controllers/paseo.js
+++ api/paseo/controllers/paseo.js
@@ -3,14 +3,14 @@ const { sanitizeEntity } = require("strapi-utils");
 module.exports = {
   async find(ctx) {
     let entities;
-    if (ctx.query["status.started"])
-      ctx.query["status.started"] = parseInt(ctx.query["status.started"]);
+    // if (ctx.query["status.started"])
+    //   ctx.query["status.started"] = parseInt(ctx.query["status.started"]);
 
     if (ctx.query._q) {
       entities = await strapi.services.paseo.search(ctx.query);
     } else {
-      //   entities = await strapi.services.paseo.find(ctx.query);
-      entities = await strapi.query("paseo").model.find(ctx.query);
+      entities = await strapi.services.paseo.find(ctx.query);
+      //   entities = await strapi.query("paseo").model.find(ctx.query);
     }
 
     return entities.map((entity) =>
