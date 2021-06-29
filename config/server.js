889795b0c177b8e14310955862a3639709b2cd23
Martin Miauro <martin@capitan.fitness>
2021-06-29 16:25:42 -0300

    try url

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const forgotPasswordTemplate = require("./email-templates/forgot-password");
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("PUBLIC_URL"),
+  url: env("PUBLIC_URL", "http://localhost:1337/"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "7dbf9c8ef079c063c02f2accf363f0ca"),
