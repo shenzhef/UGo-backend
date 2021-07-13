3df593e930ad32f79abc6a5b8b846ddcff2fa1d9
Martin Miauro <martin@capitan.fitness>
2021-07-13 11:02:26 -0300

    text notification

--- api/transaction/controllers/Notifications.js
+++ api/transaction/controllers/Notifications.js
@@ -9,39 +9,31 @@ module.exports = {
   async webhook(ctx) {
     console.log("aca", ctx.query);
 
-    // if (request.query.payment_id !== 'null') {
-    //   mercadopago.payment
-    //     .get(request.query.payment_id)
-    //     .then((pago) => {
-    //       console.log(pago.body);
-    //       fetch('https://u-go-backend-2teup.ondigitalocean.app/transactions', {
-    //         method: 'POST',
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({
-    //           payment_id: request.query.payment_id,
-    //           status: request.query.status,
-    //           total_amount: pago.body.transaction_amount,
-    //           paseador: pago.body.metadata.paseador_id,
-    //           user: pago.body.metadata.user_id,
-    //           bundleID: pago.body.metadata.bundle_id,
-    //         }),
-    //       }).then((r) => {
-    //         response.render(__dirname + '/index.html', {
-    //           payment_id: request.query.payment_id,
-    //           payment_status: request.query.status,
-    //           linking_url: request.query.linking_url,
-    //         });
-    //       });
-    //     })
-    //     .catch((e) => {
-    //       console.log('error', e);
-    //       response.redirect(request.query.linking_url);
-    //     });
-    // } else {
-    //   response.redirect(request.query.linking_url);
-    // }
+    if (ctx.query["data.id"] !== "null" && ctx.query.type == "payment") {
+      mercadopago.payment
+        .get(ctx.query["data.id"])
+        .then((pago) => {
+          console.log(pago.body);
+
+          // entity = await strapi.services.transaction.create({
+          //   payment_id: ctx.query.id,
+          //   status: request.query.status,
+          //   total_amount: pago.body.transaction_amount,
+          //   paseador: pago.body.metadata.paseador_id,
+          //   user: pago.body.metadata.user_id,
+          //   bundleID: pago.body.metadata.bundle_id,
+          // });
+
+          // return sanitizeEntity(entity, { model: strapi.models.restaurant });
+          // },
+        })
+        .catch((e) => {
+          console.log("error", e);
+          response.redirect(request.query.linking_url);
+        });
+    } else {
+      response.redirect(request.query.linking_url);
+    }
     return ctx.send(ctx.query);
   },
 };
