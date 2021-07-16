5fcf8cd8b60b4025cf4e94584a2a5148a9aa1573
Martin Miauro <martin@capitan.fitness>
2021-07-16 15:03:12 -0300

    first put transaction into paseo

--- api/transaction/controllers/Notifications.js
+++ api/transaction/controllers/Notifications.js
@@ -10,33 +10,59 @@ module.exports = {
   async webhook(ctx) {
     console.log("aca", ctx.query);
     let entity;
+    let paseo;
+    let responseMP;
     if (ctx.query["data.id"] !== "null" && ctx.query.type == "payment") {
-      mercadopago.payment
+      responseMP = mercadopago.payment
         .get(ctx.query["data.id"])
         .then(async (pago) => {
           console.log(pago.body);
+          try {
+            entity = await strapi.services.transaction.create({
+              payment_id: pago.body.id,
+              status: pago.body.status,
+              total_amount: pago.body.transaction_amount,
+              paseador: pago.body.metadata.paseador_id,
+              user: pago.body.metadata.user_id,
+              bundleID: pago.body.metadata.bundle_id,
+              date: pago.body.date_created,
+              payment_type: "mp",
+            });
+            console.log("entity", entity);
+            if (entity._id) {
+              try {
+                paseo = await strapi.services.paseo.update(
+                  {
+                    bundleID: pago.body.metadata.bundle_id,
+                  },
+                  {
+                    transaction: entity._id,
+                  }
+                );
+              } catch (error) {
+                return { error: error };
+              }
 
-          entity = await strapi.services.transaction.create({
-            payment_id: pago.body.id,
-            status: pago.body.status,
-            total_amount: pago.body.transaction_amount,
-            paseador: pago.body.metadata.paseador_id,
-            user: pago.body.metadata.user_id,
-            bundleID: pago.body.metadata.bundle_id,
-            date: pago.body.date_created,
-            payment_type: "mp",
-          });
+              console.log("paseo", paseo);
+            } else {
+              return { error: "error" };
+            }
+          } catch (error) {
+            return { error: error };
+          }
 
           // return sanitizeEntity(entity, { model: strapi.models.restaurant });
           // },
         })
         .catch((e) => {
           console.log("error", e);
+          return { error: e };
           // response.redirect(request.query.linking_url);
         });
     } else {
       // response.redirect(request.query.linking_url);
     }
+    console.log("responseMP", responseMP);
     return sanitizeEntity(entity, { model: strapi.models.transaction });
   },
 };
