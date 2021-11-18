1f9d4a785b37c88f32427bd49a8086045f6547ff
Martin Miauro <martin.miauro@gmail.com>
2021-11-18 11:12:14 -0300

    fixed notification messages 2

--- api/paseo/models/paseo.js
+++ api/paseo/models/paseo.js
@@ -11,30 +11,37 @@ const {
 
 module.exports = {
   lifecycles: {
-    async afterCreate(result) {
-      let bundle_count = await strapi.services.paseo.count({
-        bundleID: result.bundleID,
-      });
-      console.log("bundle", bundle_count);
-      if (bundle_count === 0 && result?.paseador?.notification_token) {
-        const r = send_notification([result.user.notification_token], {
-          title:
-            "Hey " + result.paseador.first_name + " han aceptado tu solicitud",
-          body: "Tienes un nuevo paseo agendado",
-        });
-        console.log(r);
-      }
-    },
+    // async afterCreate(result) {
+    //   let bundle_count = await strapi.services.paseo.count({
+    //     bundleID: result.bundleID,
+    //   });
+    //   console.log("bundle", bundle_count);
+    //   if (bundle_count === 0 && result?.paseador?.notification_token) {
+    //     const r = send_notification([result.user.notification_token], {
+    //       title:
+    //         "Hey " + result.paseador.first_name + " han aceptado tu solicitud",
+    //       body: "Tienes un nuevo paseo agendado",
+    //     });
+    //     console.log(r);
+    //   }
+    // },
     async afterUpdate(result, params, data) {
       if (data.status == 2) {
         const r = send_notification([result.user.notification_token], {
-          title: "Hey " + result.user.first_name,
-          body: "Han finalizado el paseo de " + result.dog.name,
+          title: "Paseo finalizado!😎",
+          body:
+            result.user.first_name +
+            ", han finalizado el paseo de " +
+            result.dog.name +
+            ". Gracias por elegirnos!",
         });
       } else if (data.status == 1) {
         const r = send_notification([result.user.notification_token], {
-          title: "Hey " + result.user.first_name,
-          body: "Han empezado el paseo de " + result.dog.name,
+          title: "Paseo comenzado!😁",
+          body:
+            result.user.first_name +
+            ", han empezado el paseo de " +
+            result.dog.name,
         });
       }
     },
