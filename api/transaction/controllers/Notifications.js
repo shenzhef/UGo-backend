148e4a11335b1953268fdd79e6b62f05d674d4e0
Martin Miauro <martin.miauro@gmail.com>
2021-12-05 14:58:07 -0300

    testing mp

--- api/transaction/controllers/Notifications.js
+++ api/transaction/controllers/Notifications.js
@@ -5,76 +5,9 @@ const {
   send_notification,
 } = require("../../../extensions/users-permissions/controllers/User");
 //REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
-mercadopago.configurations.setAccessToken(
-  "TEST-7917744806992209-032021-ae7dd7888dc62c96df4bbdac4fa12d19-227590603"
-);
+mercadopago.configure({
+  access_token:
+    "TEST-7917744806992209-032021-ae7dd7888dc62c96df4bbdac4fa12d19-227590603",
+});
 
-module.exports = {
-  async webhook(ctx) {
-    // console.log("aca", ctx.query);
-
-    let entity;
-    let paseo;
-    let responseMP;
-
-    if (ctx.query["data.id"] !== "null" && ctx.query.type == "payment") {
-      responseMP = mercadopago.payment
-        .get(ctx.query["data.id"])
-        .then(async (pago) => {
-          // console.log(pago.body);
-          try {
-            entity = await strapi.services.transaction.create({
-              payment_id: pago.body.id,
-              status: pago.body.status,
-              total_amount: pago.body.transaction_amount,
-              paseador: pago.body.metadata.paseador_id,
-              user: pago.body.metadata.user_id,
-              bundleID: pago.body.metadata.bundle_id,
-              date: pago.body.date_created,
-              payment_type: "mp",
-            });
-            if (entity._id) {
-              try {
-                paseo = await strapi.query("feed").model.update(
-                  {
-                    bundleID: pago.body.metadata.bundle_id,
-                  },
-                  {
-                    transaction: entity._id,
-                  },
-                  {
-                    multi: true,
-                  }
-                );
-                console.log("paseo", paseo);
-              } catch (error) {
-                console.log(error);
-                return { error: error };
-              }
-
-              return {
-                entity: entity,
-                paseo: paseo,
-              };
-            } else {
-              return { error: "error" };
-            }
-          } catch (error) {
-            return { error: error };
-          }
-        })
-        .catch((e) => {
-          console.log("error", e);
-          return { error: e };
-          // response.redirect(request.query.linking_url);
-        });
-    } else {
-      return { error: true };
-      // response.redirect(request.query.linking_url);
-    }
-    // console.log("responseMP", responseMP);
-    return responseMP;
-    ctx.send({ expo: tickets });
-    // return sanitizeEntity(entity, { model: strapi.models.transaction });
-  },
-};
+module.exports = {};
--- api/transaction/controllers/transaction.js
+++ api/transaction/controllers/transaction.js
@@ -1,16 +1,13 @@
 "use strict";
 const { sanitizeEntity } = require("strapi-utils");
 const mercadopago = require("mercadopago");
-// const fetch = require("node-fetch");
 
-//REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel/credentials
-// mercadopago.configurations.setAccessToken(
-//   "TEST-2673649138924674-062117-027521d8ee3db857ed96256a55e4dd4f-102188289"
-// );
 mercadopago.configure({
   access_token:
-    "TEST-7917744806992209-032021-ae7dd7888dc62c96df4bbdac4fa12d19-227590603",
+    "TEST-2673649138924674-062117-027521d8ee3db857ed96256a55e4dd4f-102188289",
 });
+const URL = strapi.config.server.url;
+
 module.exports = {
   async findOne(ctx) {
     const { id } = ctx.params;
@@ -58,14 +55,14 @@ module.exports = {
       external_reference: body.external_reference,
       statement_descriptor: "UGo Argentina",
       back_urls: {
-        success: strapi.config.server.url + "/transactions/feedback",
-        failure: strapi.config.server.url + "/transactions/feedback",
-        pending: strapi.config.server.url + "/transactions/feedback",
+        success: URL + "/transactions/feedback",
+        failure: URL + "/transactions/feedback",
+        pending: URL + "/transactions/feedback",
       },
       payment_methods: {
         installments: 1,
       },
-      notification_url: strapi.config.server.url + "/transactions/notification",
+      notification_url: URL + "/transactions/notification",
 
       auto_return: "approved",
     };
@@ -108,9 +105,9 @@ module.exports = {
     let mercadoPagoresponse;
     if (ctx.query.payment_id !== "null") {
       mercadoPagoresponse = await mercadopago.payment.get(ctx.query.payment_id);
-      console.log("response?", mercadoPagoresponse);
+      console.log("response?", mercadoPagoresponse.metadata);
       ctx.redirect(
-        `https://ugo.com.ar/success/?payment_id=${ctx.query.payment_id}&status=${ctx.query.status}&total_amount=${mercadoPagoresponse.total_amount}&linking_url=${mercadoPagoresponse.metadata.linking_url}`
+        `https://ugo.com.ar/success/?payment_id=${ctx.query.payment_id}&status=${ctx.query.status}&total_amount=${mercadoPagoresponse.total_amount}&linking_url=exp://127.0.0.1:19000/--/`
       );
     }
 
@@ -124,5 +121,71 @@ module.exports = {
     //   "&total_amount=500"
     //   // mercadoPagoresponse.body.transaction_amount
     // }>Volver a ugo</p>`
+  },
+  async webhook(ctx) {
+    console.log("aca", ctx.query);
+    // ctx.send("oka!");
+    let entity;
+    let paseo;
+    let responseMP;
+
+    if (ctx.query["data.id"] !== "null" && ctx.query.type == "payment") {
+      responseMP = mercadopago.payment
+        .get(ctx.query["data.id"])
+        .then(async (pago) => {
+          // console.log(pago.body);
+          try {
+            entity = await strapi.services.transaction.create({
+              payment_id: pago.body.id,
+              status: pago.body.status,
+              total_amount: pago.body.transaction_amount,
+              paseador: pago.body.metadata.paseador_id,
+              user: pago.body.metadata.user_id,
+              bundleID: pago.body.metadata.bundle_id,
+              date: pago.body.date_created,
+              payment_type: "mp",
+            });
+            if (entity._id) {
+              try {
+                paseo = await strapi.query("feed").model.update(
+                  {
+                    bundleID: pago.body.metadata.bundle_id,
+                  },
+                  {
+                    transaction: entity._id,
+                  },
+                  {
+                    multi: true,
+                  }
+                );
+                console.log("paseo", paseo);
+              } catch (error) {
+                console.log(error);
+                return { error: error };
+              }
+
+              return {
+                entity: entity,
+                paseo: paseo,
+              };
+            } else {
+              return { error: "error" };
+            }
+          } catch (error) {
+            return { error: error };
+          }
+        })
+        .catch((e) => {
+          console.log("error", e);
+          return { error: e };
+          // response.redirect(request.query.linking_url);
+        });
+    } else {
+      return { error: true };
+      // response.redirect(request.query.linking_url);
+    }
+    // console.log("responseMP", responseMP);
+    return responseMP;
+    // return sanitizeEntity(entity, { model: strapi.models.transaction });
   },
 };
