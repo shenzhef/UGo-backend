2afee2ac8e4b9f596562b08e816ddee054665a73
Martin Miauro <martin.miauro@gmail.com>
2022-02-23 14:26:10 -0300

    test

--- api/feed/controllers/Email.js
+++ api/feed/controllers/Email.js
@@ -4,6 +4,9 @@ module.exports = {
   async index(ctx) {
     console.log("entra");
     // ctx.redirect(`https://ugo.com.ar/success/?payment_id=500`);
-    //   await strapi.plugins["email"].services.email.send({
+    await strapi.plugins["email"].services.email.send({
+      to: "martin.miauro@gmail.com",
+      template_id: "d-f0c3bb03e9ac46708396083f1bb7c84d",
+    });
   },
 };
