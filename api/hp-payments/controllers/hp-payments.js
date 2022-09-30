8e363c49ce48d0cfef13dab30347688a31d914e2
Martin Miauro <martin.miauro@gmail.com>
2022-09-30 10:20:51 -0300

    advance mp house paradise

--- api/hp-payments/controllers/hp-payments.js
+++ api/hp-payments/controllers/hp-payments.js
@@ -7,7 +7,79 @@ const URL = strapi.config.server.url;
 
 module.exports = {
   async webhook(ctx) {
+    let entity;
+    let responseMP;
     console.log("ctx", ctx);
+    if (ctx.query["data.id"] !== "null" && ctx.query.type == "payment") {
+      responseMP = mercadopago.payment
+        .get(ctx.query["data.id"])
+        .then(async (pago) => {
+          console.log(pago.body);
+          try {
+            entity = await strapi.services.hp_payments.create({
+              payment_id: pago.body.id,
+              status: pago.body.status,
+              total_amount: pago.body.transaction_amount,
+              payment_type: "mp",
+              owner_name: pago.body.payer.name,
+              owner_surname: pago.body.payer.surname,
+              owner_email: pago.body.payer.email,
+              // owner_surname:
+            });
+            console.log("entity", entity);
+            // if (entity._id) {
+            //   try {
+            //     paseo = await strapi.query("reserves-hp").model.update(
+            //       {
+            //         bundleID: pago.body.metadata.bundle_id,
+            //       },
+            //       {
+            //         transaction: entity._id,
+            //       },
+            //       {
+            //         multi: true,
+            //       }
+            //     );
+            //     console.log("paseo", paseo);
+            //   } catch (error) {
+            //     console.log(error);
+            //     return { error: error };
+            //   }
+
+            //   return {
+            //     entity: entity,
+            //     paseo: paseo,
+            //   };
+            // } else {
+            //   return { error: "error" };
+            // }
+          } catch (error) {
+            return { error: error };
+          }
+        })
+        // })
+        .catch((e) => {
+          console.log("error", e);
+          return { error: e };
+          //     });
+          // } else {
+          //   return { error: true };
+        });
+    }
+    // return responseMP;
+  },
+  async backmp(ctx) {
+    // console.log("entra?");
+    console.log(ctx.query);
+
+    let mercadoPagoresponse;
+    if (ctx.query.payment_id !== "null") {
+      mercadoPagoresponse = await mercadopago.payment.get(ctx.query.payment_id);
+      ctx.redirect(
+        `https://ugo.com.ar/success/?payment_id=${ctx.query.payment_id}
+        &status=${ctx.query.status}&total_amount=${mercadoPagoresponse.body.transaction_amount}&linking_url=${mercadoPagoresponse.body.metadata.linking_url}`
+      );
+    }
   },
   async createpreference(ctx) {
     const { body } = ctx.request;
@@ -24,9 +96,9 @@ module.exports = {
       ],
       statement_descriptor: "UGo Argentina",
       back_urls: {
-        success: URL + "/hp-payments/feedback",
-        failure: URL + "/hp-payments/feedback",
-        pending: URL + "/hp-payments/feedback",
+        success: URL + "/hp-payments/backmp",
+        failure: URL + "/hp-payments/backmp",
+        pending: URL + "/hp-payments/backmp",
       },
       payment_methods: {
         installments: 1,
@@ -50,18 +122,5 @@ module.exports = {
         };
       });
     return result;
-  },
-
-  async feedback(ctx) {
-    console.log(ctx.query);
-
-    let mercadoPagoresponse;
-    if (ctx.query.payment_id !== "null") {
-      mercadoPagoresponse = await mercadopago.payment.get(ctx.query.payment_id);
-      ctx.redirect(
-        `https://ugo.com.ar/success/?payment_id=${ctx.query.payment_id}
-        &status=${ctx.query.status}&total_amount=${mercadoPagoresponse.body.transaction_amount}&linking_url=${mercadoPagoresponse.body.metadata.linking_url}`
-      );
-    }
   },
 };
